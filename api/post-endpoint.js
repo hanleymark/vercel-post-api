ffd47c4e013fe943229d39c3a6f3aab21936f906
Mark Hanley <32879360+hanleymark@users.noreply.github.com>
2025-06-18 14:03:21 +0100

    Set up simple POST endpoint

---
+++ api/post-endpoint.js
@@ -0,0 +1,15 @@
+export default function handler(req, res) {
+    if (req.method !== 'POST') {
+        return res.status(405).end('Method Not Allowed');
+    }
+
+    const token = req.headers['x-api-key'];
+    if (token !== 'your-secret-token') {
+        return res.status(403).send('Forbidden');
+    }
+
+    const data = req.body;
+    console.log('Received:', data);
+
+    res.status(200).json({ received: true });
+}
