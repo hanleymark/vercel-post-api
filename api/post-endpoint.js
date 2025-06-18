e9d454ed10d33d892c401e7a006cec587f4f64d3
Mark Hanley <32879360+hanleymark@users.noreply.github.com>
2025-06-18 14:33:56 +0100

    Log request to console

--- api/post-endpoint.js
+++ api/post-endpoint.js
@@ -1,10 +1,11 @@
 export default function handler(req, res) {
+    console.log(JSON.stringify(req));
     if (req.method !== 'POST') {
         return res.status(405).end('Method Not Allowed');
     }
 
     const token = req.headers['x-api-key'];
-    if (token !== 'your-secret-token') {
+    if (token !== 'TOPSECRET') {
         return res.status(403).send('Forbidden');
     }
 
