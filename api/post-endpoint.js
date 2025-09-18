e58a7486b79d1d5778e3066fceb676d2b46789e9
Mark Hanley <32879360+hanleymark@users.noreply.github.com>
2025-09-18 14:18:55 +0100

    Restore logging IDs to console

--- api/post-endpoint.js
+++ api/post-endpoint.js
@@ -57,10 +57,9 @@ export default function handler(req, res) {
     }
   }
   const bannerId = body?.header.bannerId;
-  //console.log(`Received CN POST request for CN ${lastProcessedId} (bannerId: ${bannerId})`);
-  console.log('Full request body:', JSON.stringify(body, null, 2));
+  console.log(`Received CN POST request for CN ${lastProcessedId} (bannerId: ${bannerId})`);
   res.status(200).json({
     success: true,
-    received: body
+    //received: body
   });
 }
